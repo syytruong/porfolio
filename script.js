e670939379fb6c99b54e3d96fa8eb7518147666a
syytruong <syytruong@gmail.com>
2021-03-02 22:11:34 +0200

    customize the send button

--- script.js
+++ script.js
@@ -36,10 +36,44 @@ window.onload = function() {
 
         emailjs.sendForm('service_458rago', 'template_fnagptr', '#contact-form')
             .then(function() {
-                alert('Email has been sent successfully!');
-                document.querySelector('#contact-form').reset();
+                // let currentColor = document.querySelector('#submit-btn').style.backgroundColor;
+                // document.querySelector('#submit-btn').style.backgroundColor = '#4BB543';
+                // document.querySelector('#submit-btn').value = 'Email Sent! ';
+                // document.querySelector('#submit-btn').style.color = '#fff';
+
+                // setTimeout(function() {
+                //     document.querySelector('#submit-btn').value = 'Send';
+                //     document.querySelector('#submit-btn').style.backgroundColor = currentColor;
+                //     document.querySelector('#contact-form').reset();
+                // }, 3000);
+                sendSuccess();
+                
             }, function(error) {
-                console.log('Cannot send email. Error: ', error);
+                alert('Cannot send email. Error: ', error);
             });
     });
+
+    function sendSuccess() {
+        let submitBtn = document.querySelector('#submit-btn');
+        if (submitBtn.classList.contains('active') || submitBtn.classList.contains('success')) {
+            return false;
+        }
+
+        submitBtn.classList.add('active');
+        setTimeout(() => {
+			submitBtn.classList.add("loader");
+            console.log(submitBtn.style.border.value);
+		}, 125);
+        setTimeout(() => {
+			submitBtn.classList.remove("loader", "active");
+			submitBtn.value = "Success!";
+			submitBtn.classList.add("success");
+		}, 1600);
+        setTimeout(() => {
+			submitBtn.value = "Send";
+			submitBtn.classList.remove("success");
+			submitBtn.blur();
+		}, 2900);
+    }
+
 }
