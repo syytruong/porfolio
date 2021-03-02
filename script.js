0abf0ac779dc083f15f116f3fe4aa7543b224801
syytruong <syytruong@gmail.com>
2021-03-02 19:18:34 +0200

    fix sending email function using EmailJs

--- script.js
+++ script.js
@@ -30,72 +30,16 @@ function setTheme(mode) {
     localStorage.setItem('theme', mode);
 }
 
-let formId = 'contact-form';
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-let  form = document.getElementById(formId);
-
-function handleForm (event) {
-    event.preventDefault();
-}
-
-form.addEventListener('submit', handleForm);
-
-let data = {
-    "access_token": "7n3ga0zsl58d0n3lgl1qsijm"
-};
-
-function onSuccess() {
-    window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
-}
-
-function onError(error) {
-    window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
-}
-
-let sendBtn = document.getElementById('submit-btn');
-
-document.getElementById('submit-btn').onclick = function sendEmail(event) {
-    event.preventDefault();
-    setTimeout(function() {
-        sendBtn.value = "Sending...";
-        sendBtn.disabled = true;
-    }, 1);
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            onSuccess();
-        } else if (request.readyState == 4) {
-            onError(request.response);
-            alert('PostMail server is down, please try later!');
-        }
-    }
-
-    let contactorName = document.querySelector('#' + formId + " [name='name']").value;
-    let subject = document.querySelector('#' + formId + " [name='subject']").value;
-    let email = document.querySelector('#' + formId + " [name='email']").value; 
-    let message = document.querySelector('#' + formId + " [name='message']").value;
-
-    data['name'] = contactorName;
-    data['subject'] = subject;
-    data['email'] = email;
-    data['message'] = message;
-
-    let params = toParams(data);
-
-    request.open("POST", "https://postmail.invotes.com/send", true);
-    request.setRequestHeader("Content-type", "application/x-wwww-form-urlencoded");
-
-    request.send(params);
-
-    return false;
-}
-
-function toParams(data) {
-    let form_data = [];
-
-    for (var key in data) {
-        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
-    }
-
-    return form_data.join('&');
+        emailjs.sendForm('service_458rago', 'template_fnagptr', '#contact-form')
+            .then(function() {
+                alert('Email has been sent successfully!');
+                document.querySelector('#contact-form').reset();
+            }, function(error) {
+                console.log('Cannot send email. Error: ', error);
+            });
+    });
 }
